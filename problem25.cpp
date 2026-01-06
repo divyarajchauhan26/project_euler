9feb0241dc425047a57ef1ef61f5162bd2173e59
divyaraj chauhan <divyaraj24204@iiitd.ac.in>
2026-01-06 20:06:09 +0530

    Update problem25.cpp

--- problem25.cpp
+++ problem25.cpp
@@ -1,38 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std;    
+using namespace std;
 
-long long fibonacci(int n) {
-    string a = "0", b = "1";
-    string c = "";
-    for(int i = 2; i <= n; i++) {
-        c = "";
+int main() {
+    vector<int> a(1, 0); // F1 = 0
+    vector<int> b(1, 1); // F2 = 1
+
+    int index = 2;
+
+    while (b.size() < 1000) {
+        vector<int> c;
         int carry = 0;
-        int len_a = a.length();
-        int len_b = b.length();
-        int max_len = max(len_a, len_b);
-        for(int j = 0; j < max_len; j++) {
-            int digit_a = (j < len_a) ? a[len_a - 1 - j] - '0' : 0;
-            int digit_b = (j < len_b) ? b[len_b - 1 - j] - '0' : 0;
-            int sum = digit_a + digit_b + carry;
-            c.push_back((sum % 10) + '0');
+
+        int n = max(a.size(), b.size());
+        for (int i = 0; i < n; i++) {
+            int x = (i < a.size()) ? a[i] : 0;
+            int y = (i < b.size()) ? b[i] : 0;
+
+            int sum = x + y + carry;
+            c.push_back(sum % 10);
             carry = sum / 10;
         }
-        if(carry) {
-            c.push_back(carry + '0');
-        }
-        reverse(c.begin(), c.end());
+
+        if (carry) c.push_back(carry);
+
         a = b;
         b = c;
+        index++;
     }
-    return c.length();
-}
-int main(){
-    int result = 0;
-    int i =0;
-    while(result < 1000){
-        result = fibonacci(i);
-        i++;
-    }
-    cout << i - 1 << endl;
+
+    cout << index << endl;
     return 0;
 }
